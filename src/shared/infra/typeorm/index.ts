9e40c56a4f3adab5511658a6c223a2fadb59c49b
Faustino J. Mateus <Faustinojmateus@gmail.com>
2023-04-06 10:33:36 +0200

    ssl true

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -17,7 +17,7 @@ const options:  any = {
     // password: "12345",
     // database: "payroll_single",
     // url: "postgres://jhkdfoxm:q_zM2AjeX07zyuWXgCpRLLEMxnX3b-v0@raja.db.elephantsql.com/jhkdfoxm",
-    url: "postgres://elint:uYQvjaysQl0dhEvkcUj8Gxe1p8jsMTLD@dpg-cgj809mbb6mo06k1h0rg-a.oregon-postgres.render.com/payroll_render",
+    url: "postgres://elint:uYQvjaysQl0dhEvkcUj8Gxe1p8jsMTLD@dpg-cgj809mbb6mo06k1h0rg-a.oregon-postgres.render.com/payroll_render?ssl=true",
     // url: "postgres://pkwqfmrt:Jag1KJ8Ug_qwj79XtDoq8pTLQlcNHV9k@babar.db.elephantsql.com/pkwqfmrt", 
     logging: true,
     synchronize: false,
