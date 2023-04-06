32981084e31f9b3509d2272615ca64eb76d44445
Faustino J. Mateus <Faustinojmateus@gmail.com>
2023-04-06 10:01:29 +0200

    single cyclic

--- dist/config/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    secret_token: "fc7a28507238c5bebe36e038cae648c5",
-    expires_in_token: "15m",
-    secret_refresh_token: "125c41faf66e2c351bff02def3b22cab",
-    expires_in_refresh_token: "30d",
-    expires_refresh_token: 30
-};
--- dist/modules/settings/dtos/ICreateSettingDTO.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// "company_name"
-// "telephone""contact""email""website""fax""company_address""company_province"
-// "company_city""postal_code""company_country"
-// "avatar""total_month_workdays""total_day_workhours"
