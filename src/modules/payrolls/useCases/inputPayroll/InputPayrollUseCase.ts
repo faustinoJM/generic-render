6f55aaf3566b8f49864cbc6930fc5360b26d5593
Faustino J. Mateus <Faustinojmateus@gmail.com>
2023-04-06 13:02:34 +0200

    flag

--- src/modules/payrolls/useCases/inputPayroll/InputPayrollUseCase.ts
+++ src/modules/payrolls/useCases/inputPayroll/InputPayrollUseCase.ts
@@ -86,7 +86,7 @@ class InputPayrollUseCase {
         const user = await this.userRepository.findById(user_id as any)
 
         if (!user) {
-          throw new  AppError("User doesn't Exists")
+          throw new  AppError("User Auth doesn't Exists")
         }
         
         const listEmployeesPayrolls: ICreatePayrollDTO2[] = [];
